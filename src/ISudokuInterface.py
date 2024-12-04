dcbe25f72ddde4b94bdc3321b8a5fa24165812c9
Stenofliege <septimus12@gmx.de>
2024-12-04 21:06:58 +0100

    Adding comments

--- src/ISudokuInterface.py
+++ src/ISudokuInterface.py
@@ -7,7 +7,7 @@ class ISudokuInterface(ABC):
 
     @abstractmethod
     def __init__(self, givenfield: list):
-        self.block_index = [0,3,6,27,30,33, 54, 57, 60]
+        self.block_index = [0,3,6,27,30,33, 54, 57, 60] #indices for upper left fields for each box, necessary for block_check
         self.fields = []
         for field in givenfield:
             if isinstance(field, int) and 0 <= field <= 9:
@@ -18,18 +18,15 @@ class ISudokuInterface(ABC):
                 raise ValueError(f"Invalid Value at position {givenfield.index(field)}")
         self.starterfield = []
         for field in self.fields:
-            self.starterfield.append(field != 0) #mirroring the information, whether the field was given from the start. True if Starterfield, false if empty (0) at start
+            self.starterfield.append(field != 0) #mirroring the information, whether the field was given from the start. True if given Starterfield, false if empty (0) at start
 
     def rc_to_index(self,row: int, column: int):
         return (row-1)* 9 + column - 1
-
 
     @abstractmethod
     def get_field_value(self, row: int, column: int) -> int:
         '''Returns the value of the field at the given row and column'''
         return self.fields[self.rc_to_index(row, column)]
-
-
 
     @abstractmethod
     def set_field_value(self, row: int, column: int, value: int) -> bool:
@@ -43,13 +40,11 @@ class ISudokuInterface(ABC):
             self.fields[self.rc_to_index(row,column)] = value
             return True
 
-            
-
     def entry_status(self, row: int, column: int, value: int) -> bool:
         '''Returns 0 if value is empty (0), 1 if value is number, 2 if value is invalid'''
         if not self.is_field_editable(row,column):
             return 2
-        elif value == "" or value == None or value == 0:
+        elif value == "" or value == None or value == 0:# allows flexible handling of entries of different types. May be removed if stricter defined in UI
             return 0
         elif not isinstance(value, int):
             return 2
@@ -72,10 +67,10 @@ class ISudokuInterface(ABC):
             return False
         possible_fields = [field for field in self.fields]
         possible_fields[self.rc_to_index(row,column)] = value
-        return self.invalid_rows(possible_fields) == {} and self.invalid_column(possible_fields) == {} and self.invalid_blocks(possible_fields) == {}
+        return self.invalid_rows(possible_fields) == {} and self.invalid_columns(possible_fields) == {} and self.invalid_blocks(possible_fields) == {}
 
 
-    def invalid_rows(self, sudoku):
+    def invalid_rows(self, sudoku: list) -> dict: # returns a dict with double numbers for each row
         invalid= {}
         for r in range(9):
             space = []
@@ -86,7 +81,7 @@ class ISudokuInterface(ABC):
                     invalid.update({r+1:sudoku[r*9+c]})
         return invalid
 
-    def invalid_column(self, sudoku):
+    def invalid_columns(self, sudoku) -> dict: # returns a dict with double numbers for each column
         invalid= {}
         for c in range(9):
             space = []
@@ -97,7 +92,7 @@ class ISudokuInterface(ABC):
                     invalid.update({r+1:sudoku[r*9+c]})
         return invalid
     
-    def invalid_blocks(self,sudoku):
+    def invalid_blocks(self,sudoku) -> dict: # returns a dict with double numbers for each block
         invalid= {}
         for b in self.block_index:
             space = []
