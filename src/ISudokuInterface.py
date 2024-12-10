bbd3f55da8b0f1fe7d1a966d584fc31c4bb2e1fe
y-decimal <83334510+y-decimal@users.noreply.github.com>
2024-12-10 21:42:47 +0100

    Make Interface output random values for testing purposes

--- src/ISudokuInterface.py
+++ src/ISudokuInterface.py
@@ -1,9 +1,9 @@
 from abc import ABC, abstractmethod
+import random
 
 
 class ISudokuInterface(ABC):
     '''Interface for Sudoku fields'''
-
 
     @abstractmethod
     def __init__(self):
@@ -14,25 +14,25 @@ class ISudokuInterface(ABC):
     @abstractmethod
     def get_field_value(self, row: int, column: int) -> int:
         '''Returns the value of the field at the given row and column'''
-        return -1
+        return random.randint(0,9)
 
 
 
     @abstractmethod
     def set_field_value(self, row: int, column: int, value: int) -> bool:
         '''Returns true if value was set, returns false if value could not be set (e.g. because it is a field that is not editable)'''
-        return False
+        return random.choice([True, False])
 
 
 
     @abstractmethod
     def is_field_editable(self, row: int, column: int) -> bool:
         '''Returns true if field is editable, returns false if field is not editable (e.g because it is a given field)'''
-        return False
+        return random.choice([True, False])
 
 
 
     @abstractmethod
     def would_value_be_valid(self, row: int, column: int, value) -> bool:
         '''Returns true if value would be valid in the field, returns false if value would not be valid (e.g. because it is already in the row, column or block)'''
-        return False
+        return random.choice([True, False])
