eb412a1ce047b1ca8d293b6920ef8caa815c4ae5
y-decimal <83334510+y-decimal@users.noreply.github.com>
2024-12-11 16:27:00 +0100

    Implement fetch and submit button callbacks in the Controller

--- src/controller/controller.py
+++ src/controller/controller.py
@@ -6,3 +6,43 @@ class Controller:
         '''Initializes the Controller'''
         self.model = model
         self.view = view
+        
+        for row in range(9):
+            for column in range(9):
+                
+                value = self.model.get_field_value(row, column)
+                if value != 0:
+                    self.view.set_field_value(row, column, value)
+                    self.view.set_field_not_editable(row, column)
+        
+    
+    
+    def fetch_button_clicked(self):
+        '''Callback for the fetch button'''
+        
+        print("Fetch Button Clicked")
+        
+        for row in range(9):           
+            for column in range(9):
+                
+                value = self.model.get_field_value(row, column)
+                
+                if value != 0: print(f"Field at row {row} and column {column} has value {value}")
+                self.view.set_field_value(row, column, value)
+                
+        
+        
+    def submit_button_clicked(self):
+        '''Callback for the submit button'''
+        
+        print("Submit Button Clicked")
+        
+        for row in range(9):
+            for column in range(9):
+                
+                value = self.view.get_field_value(row, column)
+                
+                if self.model.is_field_editable(row, column):
+                    
+                    if value != 0: print(f"Setting field at row {row} and column {column} to value {value}")
+                    self.model.set_field_value(row, column, value)
