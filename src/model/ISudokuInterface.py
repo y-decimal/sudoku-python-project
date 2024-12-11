917c95e363160cf3a15b591963c740aa4e0302bb
y-decimal <83334510+y-decimal@users.noreply.github.com>
2024-12-11 15:04:10 +0100

    Remove return statements from ISudokuInterface to prepare for dummy implementation

--- src/model/ISudokuInterface.py
+++ src/model/ISudokuInterface.py
@@ -1,5 +1,4 @@
 from abc import ABC, abstractmethod
-import random
 
 
 class ISudokuInterface(ABC):
@@ -14,25 +13,25 @@ class ISudokuInterface(ABC):
     @abstractmethod
     def get_field_value(self, row: int, column: int) -> int:
         '''Returns the value of the field at the given row and column'''
-        return random.randint(0,9)
+        pass
 
 
 
     @abstractmethod
     def set_field_value(self, row: int, column: int, value: int) -> bool:
         '''Returns true if value was set, returns false if value could not be set (e.g. because it is a field that is not editable)'''
-        return random.choice([True, False])
+        pass
 
 
 
     @abstractmethod
     def is_field_editable(self, row: int, column: int) -> bool:
         '''Returns true if field is editable, returns false if field is not editable (e.g because it is a given field)'''
-        return random.choice([True, False])
+        pass
 
 
 
     @abstractmethod
     def would_value_be_valid(self, row: int, column: int, value) -> bool:
         '''Returns true if value would be valid in the field, returns false if value would not be valid (e.g. because it is already in the row, column or block)'''
-        return random.choice([True, False])
+        pass
