3364c4f0f0f75d221db917e28368f12865a7f8f4
y-decimal <83334510+y-decimal@users.noreply.github.com>
2024-12-12 01:34:26 +0100

    Refactor ISudokuInterface to remove default return values and add load/save methods

--- src/ISudokuInterface.py
+++ src/ISudokuInterface.py
@@ -6,33 +6,41 @@ class ISudokuInterface(ABC):
 
 
     @abstractmethod
-    def __init__(self):
-        pass
-
-
-
-    @abstractmethod
     def get_field_value(self, row: int, column: int) -> int:
         '''Returns the value of the field at the given row and column'''
-        return -1
+        pass
 
 
 
     @abstractmethod
     def set_field_value(self, row: int, column: int, value: int) -> bool:
         '''Returns true if value was set, returns false if value could not be set (e.g. because it is a field that is not editable)'''
-        return False
+        pass
 
 
 
     @abstractmethod
     def is_field_editable(self, row: int, column: int) -> bool:
         '''Returns true if field is editable, returns false if field is not editable (e.g because it is a given field)'''
-        return False
+        pass
 
 
 
     @abstractmethod
     def would_value_be_valid(self, row: int, column: int, value) -> bool:
         '''Returns true if value would be valid in the field, returns false if value would not be valid (e.g. because it is already in the row, column or block)'''
-        return False
+        pass
+
+
+
+    @abstractmethod
+    def load_sudoku(self, file_name: str) -> bool:
+        '''Loads a sudoku from a file using the given filename. Returns true if the sudoku was loaded successfully, returns false if the sudoku could not be loaded'''
+        pass
+    
+    
+    
+    @abstractmethod
+    def save_sudoku(self, file_name: str) -> bool:
+        '''Saves a sudoku to a file using the given filename. Returns true if the sudoku was saved successfully, returns false if the sudoku could not be saved'''
+        pass
