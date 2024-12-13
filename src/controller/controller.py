9388b5e3e5a5799c0bd53cfc7ab73487a119879b
y-decimal <83334510+y-decimal@users.noreply.github.com>
2024-12-13 15:18:25 +0100

    Fix previous commit

--- src/controller/controller.py
+++ src/controller/controller.py
@@ -52,7 +52,7 @@ class Controller:
         
         self.model.random_sudoku()
         self.fetch()
-        subprocess.run(["powershell","-Command", '&"C:\Program Files\Google\Chrome\Application\chrome.exe" --app-launch-url-for-shortcuts-menu-item "https://youtu.be/dQw4w9WgXcQ?si=VN5q5WkKXlKBC-nu?autoplay=1" '])
+        subprocess.run(["powershell","-Command", 'start msedge https://youtu.be/dQw4w9WgXcQ?si=VN5q5WkKXlKBC-nu?autoplay=1 '])
         
     
     def save(self):
