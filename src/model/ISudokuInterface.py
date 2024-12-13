2be3a74a183d0dae95752d8f3eab884f6c8fe63b
y-decimal <83334510+y-decimal@users.noreply.github.com>
2024-12-13 20:01:54 +0100

    Add abstract methods for loading and saving sudoku in ISudokuInterface

--- src/model/ISudokuInterface.py
+++ src/model/ISudokuInterface.py
@@ -37,14 +37,16 @@ class ISudokuInterface(ABC):
         '''Generates a random sudoku'''
         pass
 
-    # @abstractmethod
-    # def load_sudoku(self, file_name: str) -> bool:
-    #     '''Loads a sudoku from a file using the given filename. Returns true if the sudoku was loaded successfully, returns false if the sudoku could not be loaded'''
-    #     pass
+
+
+    @abstractmethod
+    def load_sudoku(self, file_name: str) -> bool:
+        '''Loads a sudoku from a file using the given filename. Returns true if the sudoku was loaded successfully, returns false if the sudoku could not be loaded'''
+        pass
     
     
     
-    # @abstractmethod
-    # def save_sudoku(self, file_name: str) -> bool:
-    #     '''Saves a sudoku to a file using the given filename. Returns true if the sudoku was saved successfully, returns false if the sudoku could not be saved'''
-    #     pass
+    @abstractmethod
+    def save_sudoku(self, file_name: str) -> bool:
+        '''Saves a sudoku to a file using the given filename. Returns true if the sudoku was saved successfully, returns false if the sudoku could not be saved'''
+        pass
