8007d5619a17ec3956a04525d3a7f06e797ff729
Decimal <83334510+y-decimal@users.noreply.github.com>
2024-12-16 03:28:04 +0100

    Add function to allow importing of src modules

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,8 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+
+
 def test_placeholder():
     # This is a placeholder test
     pass
