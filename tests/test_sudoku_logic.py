ae2815a48684e1ed6c9f8fc96cc453f60e44e459
Decimal <83334510+y-decimal@users.noreply.github.com>
2024-12-18 01:10:44 +0100

    Update test_sudoku_logic.py
    Include import statements

--- tests/test_sudoku_logic.py
+++ tests/test_sudoku_logic.py
@@ -1,1 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+
+from model.SudokuLogic import SudokuLogic
+
 # Tests for SudokuLogic.py
