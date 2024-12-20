da631df4b3c49fb3ee12a4e30ec6a75cbfa12169
Decimal <83334510+y-decimal@users.noreply.github.com>
2024-12-20 08:14:29 +0100

    Add build script for packaging Sudoku App with PyInstaller (#24)
    * Add build script for packaging Sudoku App with PyInstaller
    
    * Enhance build script to check and create virtual environment, activate it, and install dependencies
    
    * Add GitHub Actions workflow for building and uploading Sudoku App package
    
    * Execute on pushes to any branch for testing
    
    * Update upload-artifact action to version 4 in build workflow
    
    * Change build environment from Ubuntu to Windows in GitHub Actions workflow
    
    * Comment out automatic requirements generation step in build workflow
    
    * Replace call to bat file with commmands themselves
    
    * Fix unexpected echo off
    
    * Remove unnecessary commands and clean up build workflow
    
    * Remove cleanup steps for internal assets in build workflow
    
    * Remove unnecessary exit command from build workflow
    
    * Update build workflow to trigger on main branch only
    
    * Move build.bat to src

---
+++ src/build.bat
@@ -0,0 +1,37 @@
+@echo off
+
+REM Change to the parent directory
+cd ..
+
+REM Check if the virtual environment directory exists
+if not exist .venv (
+    echo Error: Virtual environment directory '.venv' does not exist.
+    python -m venv .venv
+)
+
+REM Activate the virtual environment
+call .venv\Scripts\activate
+
+REM Check if the activation was successful
+if errorlevel 1 (
+    echo Error: Failed to activate the virtual environment.
+    exit /b 1
+)
+
+echo Virtual environment activated successfully.
+python -m pip install --upgrade pip
+pip install pyinstaller
+pip install -r requirements.txt
+
+REM Copy sudoku.ico to the build directory
+xcopy "assets\images\sudoku.ico" "build\" /Y
+
+pyinstaller "src/App.py" --name "Sudoku App" --clean --noconfirm --add-data "../assets:assets" --windowed --icon "sudoku.ico" --specpath "build"
+
+REM Copy the assets folder from _internal to the root of the distribution
+xcopy "dist\Sudoku App\_internal\assets" "dist\Sudoku App\assets" /E /I /Y
+rmdir /s /q "dist\Sudoku App\_internal\assets"
+del /q "build\sudoku.ico"
+
+call deactivate
+pause
