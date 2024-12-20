da75516436ba4ff9bc0413a940c185e184b4f75f
Decimal <83334510+y-decimal@users.noreply.github.com>
2024-12-20 07:34:21 +0100

    Enhance build script to check and create virtual environment, activate it, and install dependencies

--- build.bat
+++ build.bat
@@ -1,7 +1,24 @@
 @echo off
 
+REM Check if the virtual environment directory exists
+if not exist .venv (
+    echo Error: Virtual environment directory '.venv' does not exist.
+    python -m venv .venv
+)
 
-pip 3 install pyinstaller
+REM Activate the virtual environment
+call .venv\Scripts\activate
+
+REM Check if the activation was successful
+if errorlevel 1 (
+    echo Error: Failed to activate the virtual environment.
+    exit /b 1
+)
+
+echo Virtual environment activated successfully.
+python -m pip install --upgrade pip
+pip install pyinstaller
+pip install -r requirements.txt
 
 REM Copy sudoku.ico to the build directory
 xcopy "assets\images\sudoku.ico" "build\" /Y
@@ -12,4 +29,6 @@ REM Copy the assets folder from _internal to the root of the distribution
 xcopy "dist\Sudoku App\_internal\assets" "dist\Sudoku App\assets" /E /I /Y
 rmdir /s /q "dist\Sudoku App\_internal\assets"
 del /q "build\sudoku.ico"
+
+call deactivate
 pause
