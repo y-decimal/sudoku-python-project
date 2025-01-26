9787f5c4a3e354efd90146868d03b1e9e516d0fc
Decimal <83334510+y-decimal@users.noreply.github.com>
2025-01-26 09:51:15 +0100

    Add methods for getting. setting and resetting all colors

--- src/view/Settings.py
+++ src/view/Settings.py
@@ -13,6 +13,54 @@ class Colors:
     number_highlight_color_disabled = ("#bfbf00", "#FFFF00")
     
     @staticmethod
+    def get_colors():
+        return {
+            "disabled_bg_color": Colors.disabled_bg_color,
+            "disabled_text_color": Colors.disabled_text_color,
+            "enabled_bg_color": Colors.enabled_bg_color,
+            "enabled_text_color": Colors.enabled_text_color,
+            "highlight_color_enabled": Colors.highlight_color_enabled,
+            "highlight_color_disabled": Colors.highlight_color_disabled,
+            "adjacent_color_enabled": Colors.adjacent_color_enabled,
+            "adjacent_color_disabled": Colors.adjacent_color_disabled,
+            "invalid_text_color": Colors.invalid_text_color,
+            "invalid_bg_color": Colors.invalid_bg_color,
+            "number_highlight_color_enabled": Colors.number_highlight_color_enabled,
+            "number_highlight_color_disabled": Colors.number_highlight_color_disabled
+        }
+    
+    @staticmethod
+    def set_colors(colors: dict):
+        Colors.disabled_bg_color = colors["disabled_bg_color"]
+        Colors.disabled_text_color = colors["disabled_text_color"]
+        Colors.enabled_bg_color = colors["enabled_bg_color"]
+        Colors.enabled_text_color = colors["enabled_text_color"]
+        Colors.highlight_color_enabled = colors["highlight_color_enabled"]
+        Colors.highlight_color_disabled = colors["highlight_color_disabled"]
+        Colors.adjacent_color_enabled = colors["adjacent_color_enabled"]
+        Colors.adjacent_color_disabled = colors["adjacent_color_disabled"]
+        Colors.invalid_text_color = colors["invalid_text_color"]
+        Colors.invalid_bg_color = colors["invalid_bg_color"]
+        Colors.number_highlight_color_enabled = colors["number_highlight_color_enabled"]
+        Colors.number_highlight_color_disabled = colors["number_highlight_color_disabled"]
+    
+    @staticmethod
+    def reset_colors():
+        Colors.reset_disabled_bg_color()
+        Colors.reset_disabled_text_color()
+        Colors.reset_enabled_bg_color()
+        Colors.reset_enabled_text_color()
+        Colors.reset_highlight_color_enabled()
+        Colors.reset_highlight_color_disabled()
+        Colors.reset_adjacent_color_enabled()
+        Colors.reset_adjacent_color_disabled()
+        Colors.reset_invalid_text_color()
+        Colors.reset_invalid_bg_color()
+        Colors.reset_number_highlight_color_enabled()
+        Colors.reset_number_highlight_color_disabled()
+    
+    
+    @staticmethod
     def reset_disabled_bg_color():
         Colors.disabled_bg_color = ("#d0d0cd", "#2A2A2A")
     
